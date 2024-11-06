6be368c66513494de27c7bb4c40007cbaf056b31
vansh kalra <vanshkalra1379@gmail.com>
2024-11-06 23:16:01 +0530

    Added routing in backend

---
+++ Backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser";
+import mainRouter from "./router/mainRouter"
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/v1', mainRouter);
+
+
+app.listen(PORT, ()=>{
+    console.log(`Your app is running at port ${PORT}`);
+})
